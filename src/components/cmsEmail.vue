668f24c4d15f04cb7501cbf8177bfcca41ff3576
lizilong <94648929@qq.com>
2019-11-16 01:17:54 +0800

    xx

--- src/components/cmsEmail.vue
+++ src/components/cmsEmail.vue
@@ -1,110 +1,123 @@
 <!--
  * @description: Description
  * @author: lizlong<94648929@qq.com>
- * @since: 2019-09-05 13:09:31
+ * @since: 2019-11-15 17:34:24
  * @LastAuthor: lizlong
- * @lastTime: 2019-09-05 15:39:21
+ * @lastTime: 2019-11-15 21:40:09
  -->
 <template>
-	<el-autocomplete
-		v-model="email"
-		:fetch-suggestions="querySearch"
-		placeholder="请输入邮箱"
-		:trigger-on-focus="false"
-		style="width:100%;"
-		clearable
-	></el-autocomplete>
+  <div>
+    <el-date-picker
+      v-model="s_value"
+      :name="s_prop"
+      :type="s_type"
+      :time-arrow-control="b_timeArrowControl"
+      :placeholder="s_placeholder"
+      :start-placeholder="s_startPlaceholder"
+      :end-placeholder="s_endPlaceholder"
+      :editable="b_editable"
+      :format="s_format"
+      :value-format="s_valueFormat"
+      :align="s_align"
+      :range-separator="s_rangeSeparator"
+      :readonly="b_readonly"
+      :disabled="b_disabled"
+      :clearable="b_clearable"
+      class="w100"
+    ></el-date-picker>
+    <p>{{ s_value }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-	props: {
-		//值
-		value: {
-			type: String,
-			default: ""
-		},
-		//默认配置
-		restaurants: {
-			type: Array,
-			default() {
-				return [
-					{
-						value: "@qq.com"
-					},
-					{
-						value: "@163.com"
-					},
-					{
-						value: "@163.net"
-					},
-					{
-						value: "@126.com"
-					},
-					{
-						value: "@sina.com"
-					},
-					{
-						value: "@yahoo.com.cn"
-					},
-					{
-						value: "@gmail.com"
-					},
-					{
-						value: "@outlook.com"
-					}
-				];
-			}
-		}
-	},
-	data() {
-		return {
-			email: this.value
-		};
-	},
-	methods: {
-		//邮箱自动补全功能
-		querySearch(queryString, cb) {
-			//邮箱自动补全搜索方法
-			function createFilter(queryString) {
-				return restaurant => {
-					return (
-						restaurant.value
-							.toLowerCase()
-							.indexOf(queryString.toLowerCase()) === 0
-					);
-				};
-			}
-			let index = queryString.lastIndexOf("@");
-			var restaurants = this.restaurants;
-			if (index > -1) {
-				restaurants = restaurants.filter(restaurant => {
-					return (
-						restaurant.value.substr(
-							0,
-							queryString.length - index
-						) == queryString.substr(index, queryString.length)
-					);
-				});
-				restaurants = restaurants.map(restaurant => {
-					let obj = {};
-					obj.value = restaurant.value.substring(
-						queryString.length - index
-					);
-					return obj;
-				});
-			}
-			restaurants = restaurants.map(restaurant => {
-				let obj = {};
-				obj.value = queryString + restaurant.value;
-				return obj;
-			});
-			var results = queryString
-				? restaurants.filter(createFilter(queryString))
-				: restaurants;
-			// 调用 callback 返回建议列表的数据
-			cb(results);
-		}
-	}
+  name: "lz-date",
+  props: {
+    prop: {
+      type: String,
+      default: ""
+    },
+    type: {
+      type: String,
+      default: "datetime"
+    },
+    timeArrowControl: {
+      type: Boolean,
+      default: false
+    },
+    value: {
+      type: String,
+      default: ""
+    },
+    placeholder: {
+      type: String,
+      default: "选择日期"
+    },
+    startPlaceholder: {
+      type: String,
+      default: "开始日期"
+    },
+    endPlaceholder: {
+      type: String,
+      default: "结束日期"
+    },
+    editable: {
+      type: Boolean,
+      default: true
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    clearable: {
+      type: Boolean,
+      default: true
+    },
+    format: {
+      type: String,
+      default: "yyyy-MM-dd HH:mm:ss"
+    },
+    valueFormat: {
+      type: String,
+      default: "yyyy-MM-dd HH:mm:ss"
+    },
+    align: {
+      type: String,
+      default: "left"
+    },
+    rangeSeparator: {
+      type: String,
+      default: "-"
+    }
+  },
+  data() {
+    return {
+      s_prop: this.prop,
+      s_type: this.type,
+      b_timeArrowControl: this.timeArrowControl,
+      s_value: this.value,
+      s_placeholder: this.placeholder,
+      s_startPlaceholder: this.startPlaceholder,
+      s_endPlaceholder: this.endPlaceholder,
+      b_editable: this.editable,
+      b_disabled: this.disabled,
+      b_readonly: this.readonly,
+      b_clearable: this.clearable,
+      s_format: this.format,
+      s_valueFormat: this.valueFormat,
+      s_align: this.align,
+      s_rangeSeparator: this.rangeSeparator
+    };
+  }
 };
 </script>
+
+<style scoped>
+.w100 {
+  width: 100%;
+}
+</style>
